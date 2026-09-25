fc3e7841cdd7aa7d015933069ea431a27b7d2532
praveen-73 <praveenrsp25@gmail.com>
2026-09-25 21:48:41 +0530

    adsfsd

--- test.py
+++ test.py
@@ -184,26 +184,51 @@
 
 
 
-import threading
+# import threading
+# import time
+
+# def task1():
+#     for i in range(5):
+#         print("Task 1")
+#         time.sleep(1)
+
+# def task2():
+#     for i in range(5):
+#         print("Task 2")
+#         time.sleep(1)
+
+# t1 = threading.Thread(target=task1)
+# t2 = threading.Thread(target=task2)
+
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+
+# print("Both tasks completed")
+
+
+import threading 
 import time
+
+#from matplotlib.pylab import rint 
 
 def task1():
     for i in range(5):
-        print("Task 1")
+        print("task 1")
         time.sleep(1)
 
 def task2():
     for i in range(5):
-        print("Task 2")
+        print("task 2")
         time.sleep(1)
 
-t1 = threading.Thread(target=task1)
-t2 = threading.Thread(target=task2)
+t1=threading.Thread(target=task1)
+t2=threading.Thread(target=task2)
 
 t1.start()
 t2.start()
 
 t1.join()
 t2.join()
-
-print("Both tasks completed")
